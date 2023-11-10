01ff96a097a0c3d80c48771604226553a9e29d36
mikkim14 <kimkimcanaria14@gmail.com>
2023-11-10 12:13:38 +0800

    feat/list-animal-w-most-appoinment

--- veterinary.sql
+++ veterinary.sql
@@ -170,3 +170,10 @@ WHERE animalid IN (
     )
 );
 
+SELECT a.animalid, a.name, COUNT(*) AS appointment_count
+FROM animals a
+JOIN appointments ap 
+	ON a.animalid = ap.animalid
+GROUP BY a.animalid, a.name
+ORDER BY appointment_count DESC
+LIMIT 1;
